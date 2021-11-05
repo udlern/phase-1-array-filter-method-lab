73617674ab8e93decc6b774907c36dcbe067ce73
Noa Udler <udlernoa4@gmail.com>
2021-11-05 10:39:24 -0700

    commit tree 418dccae4ff7e27bf961c6c8fa81b212eaf5f988

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(array, string) {
+    const matchingListOfDrivers = array.filter(element => element.toLowerCase() === string.toLowerCase())
+    return matchingListOfDrivers;
+}
+
+function fuzzyMatch (array, string) {
+    const firstLettersMatching = array.filter(element => element[0] === string[0])
+    return firstLettersMatching
+}
+
+function matchName (array, string) {
+    const namePropertyMatching = array.filter(element => element.name === string)
+    return namePropertyMatching;
+}
